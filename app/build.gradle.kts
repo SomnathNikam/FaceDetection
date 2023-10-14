f0f2d9606d49036af73f61c4ce485e5391ef75bc
SOMNATH <somnathnikam2076@gmail.com>
2023-10-14 20:55:33 +0530

    Added ML Dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,5 +45,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-//    implementation("com.google.mlkit:face-detection:16.1.5")
+    implementation("com.google.mlkit:face-detection:16.1.5")
 }
